57d16b861c546c48adb0437d0fc1dc7de340293d
R1cardoL <andresrlopez401@gmail.com>
2023-08-15 17:49:58 -0500

    package.json updated

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor ( private gifService: GifsService){}
+
+  get tags(){
+    return this.gifService.tagsHistory;
+  }
+
+  searchTag( tag: string ):void{
+    this.gifService.searchTag( tag );
+  }
+
+  deleteList():void{
+    this.gifService.clearList();
+  }
+
+  deleteLastItem():void{
+    this.gifService.clearLastSearch();
+  }
+}
